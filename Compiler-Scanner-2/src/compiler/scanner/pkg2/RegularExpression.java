60b92cfed3fab8650c5875bb6f71fd1104d27ae1
MustafaMahmoud <mustafamabdelaziz@gmail.com>
2021-05-26 01:32:49 +0200

    Update RegularExpression.java

--- Compiler-Scanner-2/src/compiler/scanner/pkg2/RegularExpression.java
+++ Compiler-Scanner-2/src/compiler/scanner/pkg2/RegularExpression.java
@@ -13,12 +13,54 @@ import static java.lang.Character.*;
  * @author Mustafa Mahmoud
  */
 public class RegularExpression {
-    public static final String ALPHABET="a b c d e f g h i j k l m n o p q r s t u v w x y z _ " +
-            "Q W E R T Y U I O P A S D F G H J K L Z X C V B N M";
-    public static final String NUMBER=" 0 1 2 3 4 5 6 7 8 9";
-    public static boolean isAlaphabet(String token){return ALPHABET.contains(token);}
-    public static boolean isNumber(String token){return NUMBER.contains(token);}
+    public static final char[] ALPHABET={'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n' ,'o' ,'p' ,'q' ,'r' ,'s' ,'t', 'u','v', 'w', 'x', 'y', 'z', '_' ,'Q', 'W','E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J' ,'K' ,'L', 'Z', 'X', 'C', 'V' ,'B' ,'N','M'};
+    public static final char[] NUMBER={'0','1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    public static boolean isAlaphabet(char token)
+    {
+        boolean result = false;
+         for(int i=0;i<53;i++)
+        {
+            if(compare(token,ALPHABET[i])==1)
+            {
+                result = true;
+            }
+        }
+        return result;
+    }
+     public static boolean isNumber(char token)
+     {  
+         boolean result = false;
+         for(int i=0;i<10;i++)
+        {
+            if(compare(token,NUMBER[i])==1)
+            {
+                result = true;
+            }
+        }
+        return result;
+    }
     
+    public static boolean isIdentefier(String Token)
+    {
+        boolean result = false;
+        for(int i=0;i<Token.length();i++)
+        {
+            char c = Token.charAt(i);
+            if(i==0)
+            {
+                if(isNumber(c))
+                {
+                    return false;
+                }
+            }
+            else if(isNumber(c) || isAlaphabet(c))
+            {
+                result = true;
+            }
+        }
+        return result;        
+    }
     
 }
+    
     
