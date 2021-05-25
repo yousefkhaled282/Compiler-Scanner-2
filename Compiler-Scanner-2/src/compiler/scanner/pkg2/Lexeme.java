2289f3dac9fc5c774c66780be9f8a6efe113229f
yousefkhaled282 <57260853+yousefkhaled282@users.noreply.github.com>
2021-05-25 22:47:21 +0200

    add class

---
+++ Compiler-Scanner-2/src/compiler/scanner/pkg2/Lexeme.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compiler.scanner.pkg2;
+
+/**
+ *
+ * @author mode_
+ */
+import java.util.HashMap;
+import java.util.Set;
+class Lexeme {
+ HashMap<String, String> Lex = new HashMap<String, String>(); 
+ HashMap<String, String> Tok = new HashMap<String, String>(); 
+    public Lexeme() {
+    Lex.put("Divisio", "Class");
+    Lex.put("InferedFrom", "Inheritance");
+    Lex.put("WhetherDo-Else", "Condition");
+    Lex.put("Ire", "Integer");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    Lex.put("Divisio", "Class");
+    
+    }
+
+   
+
+    
+    
+}
