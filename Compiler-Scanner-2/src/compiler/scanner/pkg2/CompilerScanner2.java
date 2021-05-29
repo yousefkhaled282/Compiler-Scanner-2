f2c97b7cfae0b3f997897c9b27aff99b261e8a3a
yousefkhaled282 <57260853+yousefkhaled282@users.noreply.github.com>
2021-05-29 02:17:55 +0200

    some Updates

--- Compiler-Scanner-2/src/compiler/scanner/pkg2/CompilerScanner2.java
+++ Compiler-Scanner-2/src/compiler/scanner/pkg2/CompilerScanner2.java
@@ -39,11 +39,11 @@ public class CompilerScanner2 {
         //String s= " aj d12";
         //System.out.println(RE.isIdentefier(s));
         ArrayList<Character> lexeme = new ArrayList<>();
-        ArrayList<String> symString = new ArrayList<>();
+        ArrayList<Character> Identifier = new ArrayList<>();
         ArrayList<Character> symbol = new ArrayList<>();
         ArrayList<Integer> index = new ArrayList<>();
         Lexeme L=new Lexeme();
-        String s1= "Ire@3num=5;";
+        String s1= "@"+"reg3=reg3-1;}}";
         char [] c=s1.toCharArray();
         int len = c.length;
         ///For Loop For detect Lexeme
@@ -56,6 +56,22 @@ public class CompilerScanner2 {
            }
                
         }
+        //Bulid Lexeme and search if its found or not
+        //Lexeme ---> Token
+        StringBuilder sb = new StringBuilder();
+        for (Character ch : lexeme) {
+            
+            sb.append(ch);
+        }
+        
+        String lex = sb.toString();
+        System.out.println(lex);
+        L.isLex(lex);
+        System.out.println("Token is\t"+L.getToken(lex)); 
+       
+       
+        
+         
         ///For Loop For detect symbol and its index 
         for(int i=0;i<len;i++){
            if(L.isSymbol(c[i])){
@@ -64,24 +80,55 @@ public class CompilerScanner2 {
               symbol.add(var);
             }
         }
+        //For Loop Detect Identifier
+        if(lexeme.isEmpty()){
+            
+            index.add(0,0);
+        }
+        for(int i = 0;i<index.size()-1;i++){
+            for (int j = index.get(i)+1;j<index.get(i+1);j++){
+                char var =c[j];
+                if(!(j==index.get(i+1))){
+                Identifier.add(var);
+                }
+                
+                
+                
+            }
+            Identifier.add('/');
+        }
+        System.out.println("identifer brfore remove    "+Identifier);
+        
+        StringBuilder S_indentifier = new StringBuilder();
+        for (Character ch : Identifier) {
+            
+            S_indentifier.append(ch);
+            
+            }
+         String iden = S_indentifier.toString();
+         String [] arr= iden.split("/");
+        
+        
+        
+       
+         
+        
         //OutPuts
         
+        System.out.println("identifer after remove    "+Identifier);
+       
+         
+        System.out.println("identifer    "+S_indentifier);
+        System.out.println("identifer    "+iden);
+        System.out.println("identifer    "+arr[0]);
+        System.out.println("identifer    "+arr[1]);
+        System.out.println("identifer    "+arr[2]);
         
         System.out.println("Index of Symbols     "+index);
         System.out.println("Symbols    "+symbol);
-        System.out.println(lexeme);
         
         
-        //Bulid Lexeme and search if its found or not
-        //Lexeme ---> Token
-        StringBuilder sb = new StringBuilder();
-        for (Character ch : lexeme) {
-            sb.append(ch);
-        }
-        String lex = sb.toString();
-        System.out.println(lex);
-        L.isLex(lex);
-        System.out.println("Token is\t"+L.getToken(lex)); 
+        
         
         
         for (int i =0 ; i<symbol.size();i++){
